d00616813576a57f33558aa5cb636900ee09dcda
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2012-11-01 16:16:30 +0100

    Adds comments to help with the flow of the program

--- yabfi.ml
+++ yabfi.ml
@@ -7,16 +7,23 @@ open Batteries_uni
 
 (* (left, current, right) *)
 let parse_brainfuck_operation (l, c, r) = function
+  (* Moves to the cell in the right. *)
   | '>' -> (match r with
          | [] -> (c::l, '\000', [])
          | h::t -> (c::l, h, t))
+  (* Moves to the cell in the left. *)
   | '<' -> (match l with
            [] -> ([], '\000', c::r)
          | h::t -> (t, h, c::r))
+  (* Increments the value of the current cell. *)
   | '+' -> (l, Char.chr (Char.code c+1), r)
+  (* Decrements the value of the current cell. *)
   | '-' -> (l, Char.chr (Char.code c-1), r)
+  (* Outputs the value of the current cell. *)
   | '.' -> IO.write stdout c; (l, c, r)
+  (* Gets input for the value of the current cell. *)
   | ',' -> (l, IO.read stdin, r)
+  (* Any other character is a noop. *)
   | _ -> (l, c, r);;
 
 let rec prev_bracket n count = function
@@ -39,15 +46,20 @@ let rec next_bracket p count = function
             )
     | _ -> raise (Failure "Unmatched opening bracket");;
 
-(* (left, current, right) (previous, next*)
+(* (left, current, right) (previous, next) *)
 let rec parse_brainfuck (l, c, r) (p, n) = match n with
   | h::t -> (match h with
-          | '[' -> if c = '\000' (* Jump to matching ] *)
+          | '[' -> if c = '\000'
+                     (* Jump to next matching ]. *)
                      then let x = next_bracket (h::p) 0 t in parse_brainfuck (l, c, r) x
+                     (* Continue with the next instruction. *)
                      else parse_brainfuck (l, c, r) (h::p, t)
-          | ']' -> if c = '\000' (* Don't jump back *)
+          | ']' -> if c = '\000' 
+                     (* Continue with the next instruction. *)
                      then parse_brainfuck (l, c, r) (h::p, t)
+                     (* Jump back to previous matching [. *)
                      else let x = prev_bracket (h::t) 0 p in parse_brainfuck (l, c, r) x
+          (* Process any other character. *)
           | _ -> parse_brainfuck (parse_brainfuck_operation (l, c, r) h) (h::p, t)
           )
   | _ -> ();;
