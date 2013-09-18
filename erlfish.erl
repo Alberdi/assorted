4912f0e12650d5af5864da636abad3ba69194156
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2013-09-18 12:45:52 +0200

    Converts the mysterious integers into chars.

--- erlfish.erl
+++ erlfish.erl
@@ -5,10 +5,10 @@ interpret(L) -> read_chars(0, L).
 
 read_chars(-1, L) -> read_chars(0, L);
 read_chars(256, L) -> read_chars(0, L);
-read_chars(Value, [105|T]) -> read_chars(Value+1, T); % [i]ncrement
-read_chars(Value, [100|T]) -> read_chars(Value-1, T); % [d]ecrement
-read_chars(Value, [115|T]) -> read_chars(Value*Value, T); % [s]quare
-read_chars(Value, [111|T]) -> io:fwrite("~w~n", [Value]), read_chars(Value, T); % [o]utput
+read_chars(Value, [$i|T]) -> read_chars(Value+1, T); % [i]ncrement
+read_chars(Value, [$d|T]) -> read_chars(Value-1, T); % [d]ecrement
+read_chars(Value, [$s|T]) -> read_chars(Value*Value, T); % [s]quare
+read_chars(Value, [$o|T]) -> io:fwrite("~w~n", [Value]), read_chars(Value, T); % [o]utput
 read_chars(Value, [_|T]) -> read_chars(Value, T);
 read_chars(_,_) -> ok.
 
